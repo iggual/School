2405328e2b267541a305326abd10c3be06ac6250
iggual <130899956+iggual@users.noreply.github.com>
2025-07-05 17:13:37 +0100

    Add files via upload

---
+++ Code/welcome.c
@@ -0,0 +1,12 @@
+// Welcome to the School of Schools!
+// This is a minimal C program to print the welcome message.
+// Padding added to help GitHub detect the language reliably.
+
+#include <stdio.h>
+
+int main() {
+    printf("Welcome to the School of Schools!\n");
+    return 0;
+}
+
+/* End of program */
